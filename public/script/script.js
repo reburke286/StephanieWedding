22abd4541c67715e204b532188cbec911900f9b2
Becca Burke <reburke286@gmail.com>
2020-04-25 15:48:05 -0700

    built homepage and scaffolded file structure

---
+++ public/script/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+  $(".pic").click(function () {
+    console.log("pic button");
+  });
+  $(".gift").click(function () {
+    console.log("gift button");
+  });
+});
