a42a2f79fd50d80d34f0baf6c17c341badc87a1b
Becca Burke <reburke286@gmail.com>
2020-05-23 15:39:24 -0700

    captured input values in object

--- public/script/script.js
+++ public/script/script.js
@@ -5,5 +5,33 @@ $(document).ready(function () {
   $(".gift").click(function () {
     console.log("gift button");
   });
-  $('.datepicker').datepicker();
+  $(".datepicker").datepicker();
+
+  $(".btn-large").on("click", function (event) {
+    event.preventDefault();
+
+    const sentNoVar = $("#sentNo").prop("checked");
+
+    const sentYesVar = $("#sentYes").prop("checked");
+
+    let sent;
+    if (sentNoVar) {
+      sent = "no";
+    } else if (sentYesVar) {
+      sent = "yes";
+    } else {
+      sent = "";
+    }
+
+    const giftInfo = {
+      name: $("#name").val(),
+      gift: $("#gift").val(),
+      date: $("#date").val(),
+      address: $("#address").val(),
+      sent: sent,
+    };
+    console.log(giftInfo);
+
+    $(".sent").prop("checked", false);
+  });
 });
