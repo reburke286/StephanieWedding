0ad45c566f3787352137ae16711c3043b7c19e7b
Becca Burke <reburke286@gmail.com>
2020-06-23 09:44:21 -0700

    added sequelize structure

---
+++ models/note.js
@@ -0,0 +1,42 @@
+module.exports = function (sequelize, DataTypes) {
+  var Note = sequelize.define("Note", {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1],
+      },
+    },
+    gift: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1],
+      },
+    },
+    date: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1],
+      },
+    },
+    address: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      len: [1],
+    },
+    image: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      len: [1],
+    },
+    sent: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      len: [1],
+    },
+  });
+
+  return Note;
+};
---
+++ schema.sql
@@ -0,0 +1,4 @@
+-- Drops the blogger if it exists currently --
+DROP DATABASE IF EXISTS gifts;
+-- Creates the "gifts" database --
+CREATE DATABASE gifts;
